7696b8f49c724876a6952431758418445de9f93d
Audrey Sperry <audrey.sperry@gmail.com>
2017-09-28 10:25:29 -0400

    show all topics in database

--- src/main/java/com/audreysperry/topicvoterjpa/controllers/TopicController.java
+++ src/main/java/com/audreysperry/topicvoterjpa/controllers/TopicController.java
@@ -1,13 +1,21 @@
 package com.audreysperry.topicvoterjpa.controllers;
 
+import com.audreysperry.topicvoterjpa.dao.TopicDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TopicController {
 
+    @Autowired
+    private TopicDao topicDao;
+
     @RequestMapping("/")
-    public String topicList() {
+    public String topicList(Model model) {
+        model.addAttribute("topics", topicDao.findAll());
+
         return "index";
     }
 }
